bd3330fe3e6a4391c277359841d4ff0316d572ba
johndarknet <johndacullohaha@gmail.com>
2025-10-31 21:11:47 +0800

    Add service worker for caching assets
    Implement service worker for caching and offline support.

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = "tasklist-f-v1";
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./script.js"
+];
+
+// Install service worker and cache files
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// Fetch cached content when offline
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
